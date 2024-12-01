4442a557ef9fa6d1a57cddf08204fda3fef4c8f0
Per-Olov Jernberg <possan@possan.se>
2024-12-01 19:50:11 +0100

    Update rpi script

--- pi/color-mixer.js
+++ pi/color-mixer.js
@@ -1,34 +1,35 @@
 import WebSocket, { WebSocketServer } from "ws";
 
-// settings
-
 const RINGS = [
-  // {...}
   {
     index: 0,
     delay: 0,
-    ip: "192.168.1.102",
+    ip: "192.168.10.101",
   },
   {
     index: 0,
     delay: 200,
-    ip: "192.168.1.103",
+    ip: "192.168.10.102",
   },
   {
     index: 0,
     delay: 400,
-    ip: "192.168.1.104",
+    ip: "192.168.10.103",
   },
 ];
 
-// const OFF_PRESET = 10;
+const IDLE_PRESET_SPEC =
+  '{"on":true,"bri":128,"transition":10,"mainseg":0,"seg":[{"id":0,"start":0,"stop":294,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,49,8],[255,99,51],[0,0,0]],"fx":2,"sx":19,"ix":128,"pal":2,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"id":1,"start":294,"stop":588,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,49,8],[255,99,51],[0,0,0]],"fx":2,"sx":19,"ix":128,"pal":2,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}';
+
 const IDLE_PRESET = 10;
 const COLOR_PRESET = 11;
 const RELAY_SERVER_URL = "wss://sync.possan.codes/broadcast/dendrolux";
 const SLOW_FADE_TIME = 2.0;
 const QUICK_FADE_TIME = 0.2;
+const LIVE_TIMEOUT_TIME = 15000;
+const COLOR_SUSTAIN_TIME = 500;
 
-let lightTargetState = "idle"; // 'idle' or 'live'
+let lightTargetState = "idle";
 let lightCurrentState = "off";
 let exitLiveDeadline = 0;
 
@@ -95,30 +96,8 @@ let ringColorState = [
   },
 ];
 
-let connections = [];
-
-let connectionButtonState = [
-  //   {
-  //     id: "_1234",
-  //     expires: 0,
-  //     ring: 0,
-  //     color: [255, 0, 0],
-  //   },
-  //   {
-  //     id: "_4567",
-  //     expires: new Date().getTime() + 5000,
-  //     ring: 0,
-  //     color: [0, 255, 0],
-  //   },
-  //   {
-  //     id: "_5678",
-  //     expires: new Date().getTime() + 10000,
-  //     ring: 1,
-  //     color: [0, 0, 255],
-  //   },
-];
-
-// ring i/o
+let connectionButtonState = [];
+let buttonEvents = [];
 
 function _sendStateToRing(index, stateUpdate) {
   const req = JSON.stringify(stateUpdate);
@@ -140,24 +119,25 @@ function _sendStateToRing(index, stateUpdate) {
       console.log("OK");
     })
     .catch((e) => {
-      console.log("API error", e);
+      console.log("API error");
     });
 }
+
 function configureRingSettings(index, settingsobj) {}
 
 function configureRingPalette(index, paletteindex, paletteobj) {}
 
-function configureRingPreset(index, presetindex, presetobj) {}
+function configureRingPreset(index, presetindex, presetobj) {
+  console.log;
+}
 
 function _setRingPreset(index, presetIndex) {
   console.log("Set ring preset", index, presetIndex);
-  //   curl -X POST -d '{"ps":10}' -H "Content-Type: application/json" "http://192.168.1.102/json/state"
   _sendStateToRing(index, { ps: presetIndex });
 }
 
 function setRingColor(index, r, g, b, fadetime) {
-//   console.log("Set ring color", index, [r, g, b], fadetime);
-//   ('{"transition":1,"seg":[{"col":[[0,255,0]]},{"col":[[0,255,0]]}]}');
+  console.log("setting ring color in mode", lightCurrentState);
   _sendStateToRing(index, {
     transition: fadetime * 10,
     seg: [{ col: [[r, g, b]] }, { col: [[r, g, b]] }],
@@ -165,29 +145,12 @@ function setRingColor(index, r, g, b, fadetime) {
 }
 
 function setRingModeIdle(index) {
-//   console.log("Set ring idle", index);
   _setRingPreset(index, IDLE_PRESET);
 }
 
 function setRingModeColor(index) {
-//   console.log("Set ring off", index);
-     _setRingPreset(index, COLOR_PRESET);
+  _setRingPreset(index, COLOR_PRESET);
 }
-
-// function setRingModeColor(index) {
-//   console.log("Set ring off", index);
-//   _setRingPreset(index, COLOR_PRESET).then(() => {
-//     setRingColor(index, 0, 0, 0);
-//   });
-// }
-
-// connect to rings
-
-// run self test
-
-// connect to relay
-
-// handle messages from relay
 
 function handleRelayMessage(msg) {
   console.log("got message: " + JSON.stringify(msg));
@@ -199,57 +162,55 @@ function handleRelayMessage(msg) {
 
   if (msg.type === "enter") {
     if (msg.id && msg.color) {
-      //   colorMixerState[~~msg.id - 1] = msg.color;
-      connectionButtonState[msg._id] = {
+      buttonEvents.push({
         ring: ~~msg.id - 1,
         color: msg.color,
         expires: new Date().getTime() + 10000,
-      };
+      });
+      // connectionButtonState[msg._id] = {
+      //   ring: ~~msg.id - 1,
+      //   color: msg.color,
+      //   expires: new Date().getTime() + 10000,
+      // };
     }
   }
 
-  if (msg.type === "exit") {
-    if (msg.id) {
-      if (connectionButtonState[msg._id]) {
-        delete connectionButtonState[msg._id];
-      }
-    }
-  }
+  //   if (msg.type === "exit") {
+  //     // if (msg.id) {
+  //     //   if (connectionButtonState[msg._id]) {
+  //     //     delete connectionButtonState[msg._id];
+  //     //   }
+  //     // }
+  //   }
 
-  if (msg.type === ".disconnect") {
-    if (msg._id) {
-      if (connectionButtonState[msg._id]) {
-        delete connectionButtonState[msg._id];
-      }
-    }
-  }
+  //   if (msg.type === ".disconnect") {
+  //     // if (msg._id) {
+  //     //   if (connectionButtonState[msg._id]) {
+  //     //     delete connectionButtonState[msg._id];
+  //     //   }
+  //     // }
+  //   }
 
   if (msg.type === "ping") {
     if (msg.id) {
-      // wake up
-      //   lightTargetState = "live"; // should go to live mode
-      connectionButtonState[msg._id] = {
+      buttonEvents.push({
         ring: ~~msg.id - 1,
-        color: [0, 0, 0],
+        // color: [0, 0, 0],
         expires: new Date().getTime() + 10000,
-      };
+      });
+      // wake up
+      // lightTargetState = "live";
+      // should go to live mode
+      // connectionButtonState[msg._id] = {
+      //   ring: ~~msg.id - 1,
+      //   color: [0, 0, 0],
+      //   expires: new Date().getTime() + 10000,
+      // };
     }
   }
 }
 
-// ping rings
-
-// if more than one user connection
-
-// switch to color mixer mode
-
-// when less than one user connection return to automated program
-
 console.log("Dendrolux ring server");
-
-// first, run ring configuration and self test.
-
-// then connect to relay server
 
 const ws = new WebSocket(RELAY_SERVER_URL);
 
@@ -269,26 +230,6 @@ ws.on("message", function message(data) {
   handleRelayMessage(JSON.parse(data));
 });
 
-// function sendColorsToRings() {}
-// function updateColorMixer() {
-//   for (let k = 0; k < colorMixerState.length; k++) {
-//     let [_r, _g, _b] = colorMixerState[k];
-//     if (_r > 0) {
-//       _r -= RGB_FADESPEED;
-//     }
-//     if (_g > 0) {
-//       _g -= RGB_FADESPEED;
-//     }
-//     if (_b > 0) {
-//       _b -= RGB_FADESPEED;
-//     }
-//     _r = Math.max(0, Math.min(255, Math.round(_r)));
-//     _g = Math.max(0, Math.min(255, Math.round(_g)));
-//     _b = Math.max(0, Math.min(255, Math.round(_b)));
-//     colorMixerState[k] = [_r, _g, _b];
-//   }
-// }
-
 function evictOldNodes() {
   for (const id of Object.keys(connectionButtonState)) {
     const D = new Date().getTime() - connectionButtonState[id].expires;
@@ -300,14 +241,14 @@ function evictOldNodes() {
 }
 
 function changeLightTargetStateFromConnections() {
-  if (
-    lightTargetState === "idle" &&
-    Object.keys(connectionButtonState).length > 0
-  ) {
-    lightTargetState = "live"; // should go to live mode
-    console.log("Lights should go to live mode now.");
-    exitLiveDeadline = new Date().getTime() + 10000;
-  }
+//   if (
+//     lightTargetState === "idle" &&
+//     (Object.keys(connectionButtonState).length > 0 || buttonEvents.length > 0)
+//   ) {
+//     lightTargetState = "live"; // should go to live mode
+//     console.log("Lights should go to live mode now.");
+//     exitLiveDeadline = new Date().getTime() + LIVE_TIMEOUT_TIME;
+//   }
 
   if (lightTargetState === "live" && new Date().getTime() > exitLiveDeadline) {
     console.log("Lights should be idle now.");
@@ -316,35 +257,96 @@ function changeLightTargetStateFromConnections() {
 }
 
 function updateTargetRingColorsFromState() {
+  const T = new Date().getTime();
+
+  //   for (let k = 0; k < ringColorState.length; k++) {
+  //     let _r = 0;
+  //     let _g = 0;
+  //     let _b = 0;
+  //     // let any = 0;
+
+  //     // if (buttonEvents.length > 0){
+  //     //     let evt = buttonEvents.shift();
+  //     //     do {
+  //     //         console.log("got event", evt);
+  //     //         if (evt.ring === k) {
+  //     //             _r = evt.color[0];
+  //     //             _g = evt.color[1];
+  //     //             _b = evt.color[2];
+  //     //             any++;
+  //     //         }
+  //     //         evt = buttonEvents.shift();
+  //     //     } while (evt);
+  //     // }
+
+  //     // // for (const id of Object.keys(connectionButtonState)) {
+  //     // //   if (connectionButtonState[id].ring === k) {
+  //     // //     _r += connectionButtonState[id].color[0];
+  //     // //     _g += connectionButtonState[id].color[1];
+  //     // //     _b += connectionButtonState[id].color[2];
+  //     // //     any++;
+  //     // //   }
+  //     // // }
+
+  //     // // _r = Math.max(0, Math.min(255, Math.round(_r)));
+  //     // // _g = Math.max(0, Math.min(255, Math.round(_g)));
+  //     // // _b = Math.max(0, Math.min(255, Math.round(_b)));
+
+  //     // ringColorState[k].any = any > 0;
+  //     // if (any) {
+  //     //   ringColorState[k].lastActivity = new Date().getTime();
+  //     // }
+  //     ringColorState[k].targetColor = [_r, _g, _b];
+  //   }
+
+  //   for (let k = 0; k < ringColorState.length; k++) {
+  //     let _r = 0;
+  //     let _g = 0;
+  //     let _b = 0;
+  //     let any = 0;
+
+  //   if (buttonEvents.length > 0) {
+  // let evt = buttonEvents.shift();
+  let evt = undefined;
+  while ((evt = buttonEvents.shift())) {
+    console.log("consumed queued button event", evt);
+    if (evt.color && evt.ring !== undefined) {
+      ringColorState[evt.ring].targetColor = evt.color;
+      ringColorState[evt.ring].expires = T + COLOR_SUSTAIN_TIME;
+      // any++;
+
+      lightTargetState = "live"; // should go to live mode
+      exitLiveDeadline = new Date().getTime() + LIVE_TIMEOUT_TIME;
+    }
+    //   evt = buttonEvents.shift();
+  }
+  //   }
+
+  // for (const id of Object.keys(connectionButtonState)) {
+  //   if (connectionButtonState[id].ring === k) {
+  //     _r += connectionButtonState[id].color[0];
+  //     _g += connectionButtonState[id].color[1];
+  //     _b += connectionButtonState[id].color[2];
+  //     any++;
+  //   }
+  // }
+
+  // _r = Math.max(0, Math.min(255, Math.round(_r)));
+  // _g = Math.max(0, Math.min(255, Math.round(_g)));
+  // _b = Math.max(0, Math.min(255, Math.round(_b)));
+
+  //     ringColorState[k].any = any > 0;
+  //     if (any) {
+  //       ringColorState[k].lastActivity = new Date().getTime();
+  //     }
+  //     ringColorState[k].targetColor = [_r, _g, _b];
+  //   }
+
   for (let k = 0; k < ringColorState.length; k++) {
-    let _r = 0;
-    let _g = 0;
-    let _b = 0;
-    let any = 0;
-
-    for (const id of Object.keys(connectionButtonState)) {
-      // const D = (new Date()).getTime() -
-      if (connectionButtonState[id].ring === k) {
-        _r += connectionButtonState[id].color[0];
-        _g += connectionButtonState[id].color[1];
-        _b += connectionButtonState[id].color[2];
-        any++;
-      }
+    if (ringColorState[k].expires && ringColorState[k].expires < T) {
+      ringColorState[k].targetColor = [0, 0, 0];
+      ringColorState[k].expires = undefined;
     }
-
-    _r = Math.max(0, Math.min(255, Math.round(_r)));
-    _g = Math.max(0, Math.min(255, Math.round(_g)));
-    _b = Math.max(0, Math.min(255, Math.round(_b)));
-
-    // if (_r > 0 || _g > 0 || _b > 0) {
-    //   colorMixerState[k] = [_r, _g, _b];
-    // }
-
-    ringColorState[k].any = any > 0;
-    if (any) {
-      ringColorState[k].lastActivity = new Date().getTime();
-    }
-    ringColorState[k].targetColor = [_r, _g, _b];
   }
 }
 
@@ -354,80 +356,53 @@ function updateRingsFromState() {
     let [_lr, _lg, _lb] = ringColorState[k].liveColor;
 
     if (_lr != _tr || _lg != _tg || _lb != _tb) {
-      // console.log('Ring color changed', k, ringColorState[k])
-
       if (_tr === 0 && _tg === 0 && _tb === 0) {
-        // do slow fade to black
         console.log("slow fade to black", k, ringColorState[k].targetColor);
         setRingColor(k, _tr, _tg, _tb, SLOW_FADE_TIME);
       } else {
         console.log("quick fade to color", k, ringColorState[k].targetColor);
         setRingColor(k, _tr, _tg, _tb, QUICK_FADE_TIME);
       }
-      // } else {
-      //     console.log('No changed');
 
       ringColorState[k].liveColor = [_tr, _tg, _tb];
     }
-
-    // ringColorState[k].any = any > 0;
   }
 }
 
 function changePresetFromState() {
-  if (lightTargetState === lightCurrentState) {
-    return;
-  }
-
   if (lightTargetState === "live") {
-    lightCurrentState = "live";
+    if (lightCurrentState !== "fade-to-live" && lightCurrentState !== "live") {
+      lightCurrentState = "fade-to-live";
 
-    setTimeout(() => {
       for (var k = 0; k < RINGS.length; k++) {
         setRingModeColor(k);
-        // RINGS[k].ip
       }
-    }, 100);
 
-    // setTimeout(() => {
-    //   for (var k = 0; k < RINGS.length; k++) {
-    //     setRingModeColor(k, [0, 0, 0]);
-    //   }
-    // }, 1000);
+      setTimeout(() => {
+        lightCurrentState = "live";
+      }, 2000);
+    }
   } else if (lightTargetState === "idle") {
-    lightCurrentState = "idle";
+    if (lightCurrentState !== "fade-to-idle" && lightCurrentState !== "idle") {
+      lightCurrentState = "fade-to-idle";
 
-    setTimeout(() => {
       for (var k = 0; k < RINGS.length; k++) {
-        // setRingModeOff(k);
         setRingModeIdle(k);
       }
-    }, 100);
 
-    // setTimeout(() => {
-    //   for (var k = 0; k < RINGS.length; k++) {
-    //   }
-    // }, 1000);
+      setTimeout(() => {
+        lightCurrentState = "idle";
+      }, 1000);
+    }
   }
 }
+
 function updateRingState() {
-  //   console.log("\nUpdate ring colors");
   evictOldNodes();
   changeLightTargetStateFromConnections();
   updateTargetRingColorsFromState();
   changePresetFromState();
-  //   : '+ JSON.stringify(ringColorState))
   updateRingsFromState();
-
-  //   updateColorMixer();
-
-  //   console.log(
-  //     "Sending colors: " + JSON.stringify(colorMixerState),
-  //     lightTargetState,
-  //     connectionButtonState
-  //   );
-  //   sendColorsToRings();
-
   queueUpdateRingState();
 }
 
@@ -437,9 +412,7 @@ function queueUpdateRingState() {
   }, 100);
 }
 
-queueUpdateRingState(); // start ring loop
-
-// queue some test events.
+queueUpdateRingState();
 
 setTimeout(() => {
   handleRelayMessage({
@@ -456,6 +429,7 @@ setTimeout(() => {
     id: "1",
     color: [255, 0, 0],
   });
+
   handleRelayMessage({
     _id: "dummy2",
     type: "enter",
@@ -473,14 +447,14 @@ setTimeout(() => {
   });
 }, 6000);
 
-setTimeout(() => {
-  handleRelayMessage({ _id: "dummy1", type: "exit", id: "1" });
-}, 8000);
+// setTimeout(() => {
+//   handleRelayMessage({ _id: "dummy1", type: "exit", id: "1" });
+// }, 8000);
 
-setTimeout(() => {
-  handleRelayMessage({ _id: "dummy3", type: "exit", id: "1" });
-}, 10000);
+// setTimeout(() => {
+//   handleRelayMessage({ _id: "dummy3", type: "exit", id: "1" });
+// }, 10000);
 
-setTimeout(() => {
-  handleRelayMessage({ _id: "dummy2", type: "exit", id: "1" });
-}, 12000);
+// setTimeout(() => {
+//   handleRelayMessage({ _id: "dummy2", type: "exit", id: "1" });
+// }, 12000);
