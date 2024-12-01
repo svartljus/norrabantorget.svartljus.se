fe56d3cb5e7caa5613b539aa608b5948d9c6bb3b
Jonas Johansson <jns.johansson@gmail.com>
2024-12-01 17:43:19 +0100

    Update audio.js

--- public/audio.js
+++ public/audio.js
@@ -5,6 +5,7 @@ const startScreen = document.getElementById("startScreen");
 const startButton = document.getElementById("startButton");
 
 startButton.addEventListener("click", handleStart);
+startButton.addEventListener("touchstart", handleStart);
 
 const handleStart = () => {
     alert("");
