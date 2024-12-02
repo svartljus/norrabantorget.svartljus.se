4deb5279f7b4da6d59c1661798b4ece950180d92
Per-Olov Jernberg <possan@possan.se>
2024-12-02 17:52:28 +0100

    Update runner

--- pi/color-mixer.js
+++ pi/color-mixer.js
@@ -1,28 +1,76 @@
-import WebSocket, { WebSocketServer } from "ws";
+import WebSocket from "ws";
+
+const DELAYOFFSET = 200;
 
 const RINGS = [
   {
-    index: 0,
-    delay: 0,
-    ip: "192.168.10.101",
+    index: 1,
+    delay: DELAYOFFSET * 0,
+    ip: "192.168.10.201",
   },
   {
-    index: 0,
-    delay: 200,
-    ip: "192.168.10.103",
+    index: 2,
+    delay: DELAYOFFSET * 1,
+    ip: "192.168.10.202",
   },
   {
-    index: 0,
-    delay: 400,
-    ip: "192.168.10.100",
+    index: 3,
+    delay: DELAYOFFSET * 2,
+    ip: "192.168.10.203",
+  },
+  {
+    index: 4,
+    delay: DELAYOFFSET * 3,
+    ip: "192.168.10.204",
+  },
+  {
+    index: 5,
+    delay: DELAYOFFSET * 4,
+    ip: "192.168.10.205",
+  },
+
+  {
+    index: 6,
+    delay: DELAYOFFSET * 5,
+    ip: "192.168.10.206",
+  },
+  {
+    index: 7,
+    delay: DELAYOFFSET * 6,
+    ip: "192.168.10.207",
+  },
+  {
+    index: 8,
+    delay: DELAYOFFSET * 7,
+    ip: "192.168.10.208",
+  },
+  {
+    index: 9,
+    delay: DELAYOFFSET * 8,
+    ip: "192.168.10.209",
+  },
+  {
+    index: 10,
+    delay: DELAYOFFSET * 9,
+    ip: "192.168.10.210",
+  },
+  {
+    index: 11,
+    delay: DELAYOFFSET * 10,
+    ip: "192.168.10.211",
+  },
+  {
+    index: 13,
+    delay: DELAYOFFSET * 11,
+    ip: "192.168.10.212",
   },
 ];
 
 const COLOR_PRESET_SPEC =
-  '{"on":true,"bri":128,"transition":10,"mainseg":0,"seg":[{"id":0,"start":0,"stop":294,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[255,0,0],[0,0,255]],"fx":0,"sx":128,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"id":1,"start":294,"stop":588,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[0,0,0],[0,0,0]],"fx":0,"sx":128,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}';
+  '{"on":true,"bri":128,"transition":10,"mainseg":0,"seg":[{"id":0,"start":0,"stop":320,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[255,0,0],[0,0,255]],"fx":0,"sx":128,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"id":1,"start":320,"stop":640,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[0,0,0],[0,0,0],[0,0,0]],"fx":0,"sx":128,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}';
 
 const IDLE_PRESET_SPEC =
-  '{"on":true,"bri":128,"transition":10,"mainseg":0,"seg":[{"id":0,"start":0,"stop":294,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,49,8],[255,99,51],[0,0,0]],"fx":2,"sx":19,"ix":128,"pal":2,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"id":1,"start":294,"stop":588,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[255,49,8],[255,99,51],[0,0,0]],"fx":2,"sx":19,"ix":128,"pal":2,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":1},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}';
+  '{"on":true,"bri":128,"transition":5,"mainseg":0,"seg":[{"id":0,"start":0,"stop":320,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[228,83,21],[255,143,74],[0,0,255]],"fx":2,"sx":71,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"id":1,"start":320,"stop":640,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"set":0,"col":[[228,83,21],[255,143,74],[0,0,0]],"fx":2,"sx":71,"ix":128,"pal":0,"c1":128,"c2":128,"c3":16,"sel":true,"rev":false,"mi":false,"o1":false,"o2":false,"o3":false,"si":0,"m12":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]}';
 
 const IDLE_PRESET = 10;
 const COLOR_PRESET = 11;
@@ -119,7 +167,7 @@ function _sendStateToRing(index, stateUpdate) {
   })
     .then((r) => r.json())
     .then((r) => {
-      console.log("OK");
+      // console.log("OK");
     })
     .catch((e) => {
       console.log("API error");
@@ -142,7 +190,7 @@ function _setRingPreset(index, presetIndex) {
 }
 
 function setRingColor(index, r, g, b, fadetime) {
-  console.log("setting ring color in mode", lightCurrentState);
+  // console.log("setting ring color in mode", lightCurrentState);
   _sendStateToRing(index, {
     transition: fadetime * 10,
     seg: [{ col: [[r, g, b]] }, { col: [[r, g, b]] }],
@@ -394,12 +442,12 @@ function changePresetFromState() {
       lightCurrentState = "fade-to-idle";
 
       for (var k = 0; k < RINGS.length; k++) {
-        setRingModeIdle(k);
+        setTimeout(setRingModeIdle.bind(this, k), RINGS[k].delay);
       }
 
       setTimeout(() => {
         lightCurrentState = "idle";
-      }, 1000);
+      }, 3000);
     }
   }
 }
@@ -424,6 +472,45 @@ async function delay(ms) {
 }
 
 async function init() {
+  // configure exit handler
+
+  process.stdin.resume(); // so the program will not close instantly
+
+  async function exitHandler(options, exitCode) {
+    console.log("in Exit handler, fading out lights...", options, exitCode);
+
+    for (var k = 0; k < RINGS.length; k++) {
+      await setRingModeColor(k);
+    }
+
+    await delay(3000);
+
+    for (var k = 0; k < RINGS.length; k++) {
+      await setRingColor(k, 0, 0, 0, 1);
+    }
+
+    await delay(3000);
+
+    console.log("lights should be off now.");
+
+    if (options.exit) {
+      process.exit();
+    }
+  }
+
+  // do something when app is closing
+  process.on("exit", exitHandler.bind(null, { cleanup: true }));
+
+  // catches ctrl+c event
+  process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+
+  // catches "kill pid" (for example: nodemon restart)
+  process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+  process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
+
+  // catches uncaught exceptions
+  process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
+
   // configure all presets
 
   console.log("Dendrolux ring server");
@@ -463,7 +550,10 @@ async function init() {
 
   console.log("Starting rings idle mode.");
   for (var k = 0; k < RINGS.length; k++) {
-    await setRingModeIdle(k);
+    // await setRingModeIdle(k);
+    for (var k = 0; k < RINGS.length; k++) {
+      setTimeout(setRingModeIdle.bind(this, k), RINGS[k].delay);
+    }
   }
 
   await delay(3000);
@@ -471,39 +561,39 @@ async function init() {
   console.log("Start main loop.");
   queueUpdateRingState();
 
-  console.log("Trigger simulated events.");
+  // console.log("Trigger simulated events.");
 
-  handleRelayMessage({
-    _id: "dummy1",
-    type: "ping",
-    id: "1",
-  });
+  // handleRelayMessage({
+  //   _id: "dummy1",
+  //   type: "ping",
+  //   id: "1",
+  // });
 
-  await delay(2000);
+  // await delay(2000);
 
-  handleRelayMessage({
-    _id: "dummy1",
-    type: "enter",
-    id: "1",
-    color: [255, 0, 0],
-  });
+  // handleRelayMessage({
+  //   _id: "dummy1",
+  //   type: "enter",
+  //   id: "1",
+  //   color: [255, 0, 0],
+  // });
 
-  handleRelayMessage({
-    _id: "dummy2",
-    type: "enter",
-    id: "2",
-    color: [255, 0, 0],
-  });
-  await delay(2000);
+  // handleRelayMessage({
+  //   _id: "dummy2",
+  //   type: "enter",
+  //   id: "2",
+  //   color: [255, 0, 0],
+  // });
+  // await delay(2000);
 
-  handleRelayMessage({
-    _id: "dummy3",
-    type: "enter",
-    id: "1",
-    color: [0, 255, 0],
-  });
+  // handleRelayMessage({
+  //   _id: "dummy3",
+  //   type: "enter",
+  //   id: "1",
+  //   color: [0, 255, 0],
+  // });
 
-  await delay(2000);
+  await delay(1000);
 
   console.log("Connect to messaging server.");
   connectToRelay();
